e93cb33c8aeff20fe00b997047a37aaca0c43972
Xiang Li <lostxine@gmail.com>
2022-09-05 22:14:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# XboxControllerRelay
+
+
+## How does it work?
+
+```
+[Xbox Controller] =Wire/BT=> sender.py =UDP=> receiver.py
+```
+
+## Installation
+
+On sender
+```
+pip install inputs
+```
---
+++ sender.py
@@ -0,0 +1,110 @@
+from inputs import get_gamepad
+import math
+import threading
+import pickle
+
+# borrowed from https://github.com/kevinhughes27/TensorKart/blob/master/utils.py
+
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+        
+        
+    def encode(self):
+        data = {i: self.__dict__[i] for i in self.__dict__ if not i.startswith('_')}
+        return pickle.dumps(data)
+    
+        
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.X = event.state
+                elif event.code == 'BTN_WEST':
+                    self.Y = event.state
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY1':
+                    self.LeftDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY2':
+                    self.RightDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY3':
+                    self.UpDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY4':
+                    self.DownDPad = event.state
+                    
+                    
+if __name__ == '__main__':
+    import time
+    import socket
+    
+    xbox = XboxController()
+    
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    
+    while True:
+        try:
+            value = xbox.encode()
+            sock.sendto(value, (UDP_IP, UDP_PORT))
+            time.sleep(1 / 20)
+        except KeyboardInterrupt:
+            break
+    
+        
